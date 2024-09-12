2853ac5ff26cd2fc44a35ce254dfb921b81e7959
AdamNizam <AdmNizamwekaweka37@gmail.com>
2024-09-12 20:39:57 +0700

    use DrawArc for create cicrle

--- app/src/main/java/com/example/likesapp/MainActivity.kt
+++ app/src/main/java/com/example/likesapp/MainActivity.kt
@@ -1,20 +1,56 @@
 package com.example.likesapp
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.likesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val mBitmap = Bitmap.createBitmap(1000, 1000, Bitmap.Config.ARGB_8888)
+    private val mCanvas = Canvas(mBitmap)
+    private val mPaint = Paint()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.imageView.setImageBitmap(mBitmap)
+        showFace()
+    }
+
+    private val halfOfWidth = (mBitmap.width/2).toFloat()
+    private val halfOfHeight = (mBitmap.height/2).toFloat()
+
+    private val left = 150F
+    private val top = 250F
+    private val right = mBitmap.width - left
+    private val bottom = mBitmap.height.toFloat() - 50F
+
+    private fun showFace() {
+        val face = RectF(left, top, right, bottom)
+
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.yellow_left_skin, null)
+        mCanvas.drawArc(face, 90F, 180F, false, mPaint)
+
+        mPaint.color = ResourcesCompat.getColor(resources, R.color.yellow_right_skin, null)
+        mCanvas.drawArc(face, 270F, 180F, false, mPaint)
+
     }
 }
